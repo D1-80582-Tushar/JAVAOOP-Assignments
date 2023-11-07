35e92cef1ba15787b90942ad379321a12bcfdf88
tushar <sunbeamtushar@gmail.com>
2023-11-07 17:09:50 +0530

    assignment-16-q1,2

---
+++ Assignment-16/Question1/src/com/q1/Books.java
@@ -0,0 +1,146 @@
+package com.q1;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Scanner;
+
+class PriceBookComparaterDesc implements Comparator<Book>{
+
+	@Override
+	public int compare(Book obj1, Book obj2) {
+		// TODO Auto-generated method stub
+		int diff = Double.compare(obj1.getPrice(), obj2.getPrice());
+		return -diff;
+	}
+	
+}
+
+public class Books {
+//    Books[
+	private static ArrayList<Book> books = new ArrayList<>();
+	 
+	    
+	 
+	public static ArrayList<Book> loadData() {
+		books.add(new Book("2", 29.99, "John Doe", 15));
+		books.add(new Book("1", 19.95, "Jane Smith", 20));
+		books.add(new Book("3", 49.99, "Bob Johnson", 8));
+		books.add(new Book("4", 9.99, "Alice Brown", 10));
+	    books.add(new Book("5", 35.50, "David Lee", 30));
+        return books;
+    }
+	
+	public static void addBook(ArrayList<Book> books,Scanner sc) {
+	    String isbn;
+
+		Book b = null;
+	    
+		System.out.println("Enter Book Isbn");
+        isbn = sc.next();
+        Book testobj = new Book();
+        testobj.setIsbn(isbn);
+        
+        int index = books.indexOf(testobj);
+        if(index!=-1) {
+        	System.out.println("Book is already present !! adding to quantity");
+			b = books.get(index);
+			b.setQuantity(b.getQuantity()+1);
+		}else {
+			b = new Book();
+			b.setIsbn(isbn);
+			System.out.println("Enter Book Price");
+			b.setPrice(sc.nextDouble());
+			System.out.println("Enter Book Author Name");
+			b.setAuthorName(sc.next());
+			System.out.println("Enter Book Quantity");
+			b.setQuantity(sc.nextInt());
+			books.add(b);			
+		}
+   }
+   
+   public static void displayBooks(Iterator<Book> e,ArrayList<Book> books) {
+        Collections.sort(books);
+        for (Book book : books) {
+			System.out.println(book);
+		}
+   }
+   
+   public static void deleteBook(Iterator<Book> e,Collection<Book> books,Scanner sc) {
+	   System.out.println("Enter book isbn");
+		String id = sc.next();
+		 e = books.iterator();
+		while(e.hasNext()) {
+		 Book ele = e.next();
+			if(ele.getIsbn().equals(id)) // note carefully (book.getIsbn() == id) will only check for references this won't work
+			{ 
+				 e.remove(); //good to use iterator e in order to maintain consistency between collection and iterator
+			}
+		}
+   }
+   
+   public static Book isBookExistReturnBook(ArrayList<Book> books,String id) {
+	   Book testobj = new Book();
+       testobj.setIsbn(id);
+       
+       int index = books.indexOf(testobj);
+       if(index!=-1) {
+    	   return books.get(index);
+       }else {
+    	   return null;
+       }
+   }
+   
+   public static void deleteAllBooks(Collection<Book> books) throws InterruptedException {
+		System.out.println("Deleting...");
+		Thread.sleep(2000);
+		books.removeAll(books);
+		System.out.println("Successfully deleted.");
+   }
+   
+   public static void displayBooksInPriceOrderDesc(ArrayList<Book> books) {
+	   Collections.sort(books,new PriceBookComparaterDesc());
+	   for (Book book : books) {
+			System.out.println(book);
+		} 
+   }
+   
+   public static void reverseBookList(ArrayList<Book> books) {
+	     Collections.reverse(books);
+	     for (Book book : books) {
+				System.out.println(book);
+			} 
+   }
+	public static void saveBooks(ArrayList<Book> books) throws IOException {
+		  try(FileOutputStream fos = new FileOutputStream("book.txt")){
+			  try(ObjectOutputStream dos = new ObjectOutputStream(fos)){  
+				  for (Book book : books) {
+					  dos.writeObject(book);
+				  }  
+			  }			  
+		  }
+	}
+	public static void readFileBook() throws FileNotFoundException, IOException {
+		try(FileInputStream fin = new FileInputStream("book.txt")){
+			try(ObjectInputStream dis = new ObjectInputStream(fin)){
+                while(true) {
+                	Book b = (Book) dis.readObject();    
+                	System.out.printf("%s %.2f %s %d \n", b.getIsbn() , b.getPrice() , b.getAuthorName() , b.getQuantity());
+                }
+			}
+
+		}catch(Exception e) {
+//			e.printStackTrace();
+		}
+	}
+}
---
+++ Assignment-16/Question2/src/com/q2/Tester.java
@@ -0,0 +1,81 @@
+package com.q2;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Tester {
+    
+	private static int menu(Scanner sc) {
+		int choice = 0;
+		System.out.println("1. Add new book in list.");
+		System.out.println("2. Display all books in forward order.");
+		System.out.println("3. Delete book of given id");
+		System.out.println("4. Check if book with given isbn is in list or not");
+		System.out.println("5. Delete all books in list");
+		System.out.println("6. Sort books by price in desc order");
+		System.out.println("7. Reverse the list");
+		System.out.println("8. Save books in file");
+		System.out.println("9. Load books from file");
+		System.out.println("0 - EXIT");
+		choice = sc.nextInt();
+		return choice;
+	}
+	
+
+//	. In menu-driven program of Books (with ArrayList - Day12 Q1) add two more menus. Save books in ﬁle and load books from ﬁle. Use DataOutputStream
+//	and DataInputStream.
+	
+	public static void main(String[] args) throws InterruptedException, IOException {
+		// TODO Auto-generated method stub
+	
+		ArrayList<Book> books = new ArrayList<>();
+		Iterator<Book> e = null;
+        books = Books.loadData();
+		Scanner sc = new Scanner(System.in);
+		
+	    int choice;
+		while((choice=menu(sc))!=0) {
+		     switch (choice) {
+			case 1://add a book
+				Books.addBook(books,sc);
+				break;
+			case 2://display all books
+				Books.displayBooks(e,books);
+				break;
+			case 3://delete book of given id
+				Books.deleteBook(e,books,sc);
+				break;
+			case 4://check if book exist
+				System.out.println("Enter book isbn");
+				String id = sc.next();
+			    Book b = Books.isBookExistReturnBook(books,id);
+			    if(b!=null) {
+			    	System.out.println(b);
+			    }else {
+			    	System.out.println("Not Found");
+			    }
+				break;
+			case 5:
+			    Books.deleteAllBooks(books);
+				break;
+			case 6:
+	            Books.displayBooksInPriceOrderDesc(books);
+	            break;
+			case 7:
+				Books.reverseBookList(books);
+				break;
+			case 8:
+				Books.saveBooks(books);
+				break;
+			case 9:
+				Books.readFileBook();
+				break;
+			default:
+				break;
+			}	
+		}
+	}
+
+}
